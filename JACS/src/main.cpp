7fba6a0d2fa98b824a8c4c94ae308c25c7974b24
Vasily Markov <mvv1993vasya@gmail.com>
2026-09-22 21:37:11 +0000

    Add skeleton

--- JACS/src/main.cpp
+++ JACS/src/main.cpp
@@ -1,5 +1,40 @@
 #include <print>
+#include <vector>
+#include <optional>
+#include <unordered_set>
+#include <unordered_map>
+#include <memory>
+
+enum class NodeId: uint32_t {};
+enum class BranchId: uint32_t {};
+
+constexpr NodeId k_ground{};
+
+struct Resistor  { NodeId a, b; double r; };
+struct Vsource   { NodeId a, b; double v; BranchId k; };
+
+class NetList {
+public:
+    NodeId node(std::string_view name);
+
+    void add(Resistor resistor) {
+        std::get<std::vector<Resistor>>(elements_).push_back(resistor);
+    }
+private:
+    std::tuple<std::vector<Resistor>, std::vector<Vsource>> elements_;
+};
+
+
+class MNASystem {
+public:
+    void addAdmittance(NodeId a, NodeId b, double y);
+private:
+};
+
+void stamp(const Resistor& resistor, MNASystem& mna) {
+    mna.addAdmittance(resistor.a, resistor.b, 1.0 / resistor.r);
+}
 
 int main() {
-    std::println("Hello, world!");
+    
 }
